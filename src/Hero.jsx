d6ce3537dff032bbdb6bcd014e0182e6e2ac2cfe
ABISHEK <abideepu19.vrs@gmail.com>
2024-07-03 11:14:08 +0530

    Update Dir new
    Changed the dir of all the imgs to ./public/assets/

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import heroPicture from "public\assets\hero-pic.png"
 export default function Hero(){
     return (
         <section className="hero-section">
             <div className="hero-img">
-                <img src={heroPicture} className="hero-pic" alt="marquee images" />
+                <img src={`./public/assets/hero-pic.png`} className="hero-pic" alt="marquee images" />
             </div>
             <h1 className="hero-header">Start Gaming Now !</h1>
             <p className="hero-text">Dive into the world of gaming with these awesome 
