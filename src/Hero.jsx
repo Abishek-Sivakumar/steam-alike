935f8c15a5b9bd7784f46893ff0aa32db484165b
ABISHEK <abideepu19.vrs@gmail.com>
2024-07-03 11:07:19 +0530

    Update Hero src

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,10 @@
 import React from "react"
-
+import heroPicture from "public\assets\hero-pic.png"
 export default function Hero(){
     return (
         <section className="hero-section">
             <div className="hero-img">
-                <img src={`public\assets\hero-pic.png`} className="hero-pic" alt="marquee images" />
+                <img src={heroPicture} className="hero-pic" alt="marquee images" />
             </div>
             <h1 className="hero-header">Start Gaming Now !</h1>
             <p className="hero-text">Dive into the world of gaming with these awesome 
