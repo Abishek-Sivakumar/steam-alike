b94665071aec25c228604099e0d1fea09f268653
ABISHEK <abideepu19.vrs@gmail.com>
2024-07-03 08:48:38 +0530

    Update dir
    Change the directories of the like and hero pics

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import like from "src\assets\like.png"
 
 export default function Card(props){
     let tagText
@@ -12,7 +13,7 @@ export default function Card(props){
             {tagText && <div className="tagDiv">{tagText}</div>}
             <img className="game-pic" src={`src/assets/${props.item.imgSrc}`}/>
             <div className="game-stats">
-               <img className="like-pic" src={"src/assets/like.png"}/>
+               <img className="like-pic" src={like}/>
                <p className="rating-mode">{props.item.stats.rating}({props.item.stats.reviewCount})/{props.item.stats.mode}</p>
             </div>
             <p className="game-name"><strong>{props.item.name}</strong></p>
