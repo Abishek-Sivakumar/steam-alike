d6c03fdbea52da48d1609997242256e734bf8f6a
ABISHEK <abideepu19.vrs@gmail.com>
2024-07-08 22:05:53 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,15 @@
 import React from "react"
 import like from "/public/images/assets/like.png"
-import godOfWar from "/public/images/assets/god.png"
+import god from "/public/images/assets/god.png"
+import cyberpunk from "/public/images/assets/cyberpunk.png"
+import eldenring from "/public/images/assets/eldenring.png"
+import got from "/public/images/assets/got.png"
+import gtav from "/public/images/assets/gtav.png"
+import hitman from "/public/images/assets/hitman.png"
+import rdr2 from "/public/images/assets/rdr2.png"
+import re4 from "/public/images/assets/re4.png"
+import sekiro from "/public/images/assets/sekiro.png"
+import witcher3 from "/public/images/assets/witcher3.png"
 
 export default function Card(props){
     let tagText
@@ -8,12 +17,35 @@ export default function Card(props){
         tagText="Top Rated"
     }
 
+    let cardImage=""
+
+    if(props.item.id==1){
+        cardImage=got
+    }else if(props.item.id==2){
+        cardImage=god
+    }
+    else if(props.item.id==3){
+        cardImage=eldenring
+    }else if(props.item.id==4){
+        cardImage=gtav
+    }else if(props.item.id==5){
+        cardImage=hitman
+    }else if(props.item.id==6){
+        cardImage=re4
+    }else if(props.item.id==7){
+        cardImage=rdr2
+    }else if(props.item.id==8){
+        cardImage=cyberpunk
+    }else if(props.item.id==9){
+        cardImage=sekiro
+    }
+
     //`/public/images/assets/${props.item.imgSrc}`
     
     return (
         <div className="game-div">
             {tagText && <div className="tagDiv">{tagText}</div>}
-            <img className="game-pic" src={godOfWar}/>
+            <img className="game-pic" src={cardImage}/>
             <div className="game-stats">
                <img className="like-pic" src={like}/>
                <p className="rating-mode">{props.item.stats.rating}({props.item.stats.reviewCount})/{props.item.stats.mode}</p>
